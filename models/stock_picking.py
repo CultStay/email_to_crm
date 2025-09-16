1fd2c30d7b7eb98fef5d7c5847216baadb8e5760
CultStay <odoo@ip-172-31-32-146.ap-south-1.compute.internal>
2025-09-16 15:25:09 +0530

    [FIX] Fixed location automatically setting

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -6,6 +6,6 @@ class StockPicking(models.Model):
     @api.model
     def create(self, vals):
         sales_man = self.env['stock.location'].search([('name', '=', 'SALES MAN')], limit=1)
-        if sales_man:
+        if sales_man and not vals.get('location_id'):
             vals['location_id'] = sales_man.id
         return super().create(vals)

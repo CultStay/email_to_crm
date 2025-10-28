0a350f3ad7d7cc42a428f0fd31cea1123556f1ea
Sudarsanan12 <sudarsanan1996@gmail.co>
2025-10-28 23:22:43 +0530

    [FIX] Fixed sale order confirm issue

--- email_to_crm/models/stock_picking.py
+++ email_to_crm/models/stock_picking.py
@@ -6,6 +6,7 @@ class StockPicking(models.Model):
     @api.model_create_multi
     def create(self, vals):
         sales_man = self.env['stock.location'].search([('name', '=', 'SALES MAN')], limit=1)
-        if sales_man and not vals.get('location_id'):
-            vals['location_id'] = sales_man.id
+        for val in vals:
+            if sales_man and not val.get('location_id'):
+                val['location_id'] = sales_man.id
         return super().create(vals)

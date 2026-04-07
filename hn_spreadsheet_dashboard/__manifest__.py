9608982e26bc0297929f0182bb36353138ae4f5e
Harpritsinh Sisodiya <sh851995@gmail.com>
2026-04-07 20:58:05 +0530

    [ADD] add new module hn_spreadsheet_dashboard

---
+++ hn_spreadsheet_dashboard/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Spreadsheet Dashboard Enhancement',
+    'category': 'Dashboard',
+    'summary': 'This module is used for spreadsheet dashboard enhancement.',
+    'version': '18.0.0.0.0',
+    'author': 'Harprit',
+    'license': 'LGPL-3',
+    'description': """This module is used for spreadsheet dashboard enhancement.""",
+    'depends': ['spreadsheet_dashboard_sale'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+}
